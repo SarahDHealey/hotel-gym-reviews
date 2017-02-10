f20eef092ff53b99834370f666b9588ba619da43
Sarah Healey <s.deane.healey@gmail.com>
2017-02-09 20:41:30 -0700

    reformatted post from array to an object with props

--- scripts/component.js
+++ scripts/component.js
@@ -7,21 +7,21 @@
   })
 
   function controller() {
-  const vm = this
+    const vm = this
 
+    vm.$onInit = function () {
+      vm.showNewPostForm = false;
+    }
 
-  vm.$onInit = function () {
-    vm.showNewPostForm = false;
-  }
-
-  vm.formatPost = function () {
-  let postParts = [vm.title]
-  postParts.push(vm.author)
-  postParts.push(vm.image)
-  postParts.push(vm.description)
-  vm.formattedPost = postParts.join(' ')
-  console.log(vm)
-  }
+    vm.formatPost = function () {
+    let post = {};
+    post.title = vm.title;
+    post.author = vm.author;
+    post.image = vm.image;
+    post.description = vm.description;
+    vm.formattedPost = post;
+    console.log(vm.formattedPost)
+    }
   }
 
 }());
